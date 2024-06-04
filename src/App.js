86e35e47334bdcff40c7cf5a1ce3572c2aae2878
shadowfax29 <76819816+shadowfax29@users.noreply.github.com>
2024-06-04 21:06:13 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Register from "./component/register";
-
+import { Toaster } from 'alert';
 import Login from './component/login';
 import Account from './component/account';
 import PrivateRoutes from './component/privateroute';
@@ -10,6 +10,7 @@ import Product from './component/products';
 function App() {
   return (
     <div>
+         <Toaster position='top-center'/>
       <Routes>
         <Route path="/" element={<Navigate to="/register" />} /> 
         <Route path="/register" element={<Register />} />
